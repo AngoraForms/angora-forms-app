5ebfba2514503139906c05fe1eedfb75ad3757eb
achen9149 <aaronchen9149@gmail.com>
2023-05-23 15:20:41 -0400

    reconfigured prisma db to elephant sql without migration

--- prisma/migrations/20230523181626_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Account" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "username" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
