ec2f9375d921adb665e6d4f93f1c50f1f6b0fac7
Curtis.Lovrak <curtislovrak@gmail.com>
2023-05-25 15:08:05 -0400

    did some more styling to the login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -15,18 +15,18 @@ export default function Login() {
   return (
     <div>
       <div className = "min-h-screen grid place-content-center">
-      <p className="text-lg mb-2" >Login</p>
+      <p className="text-3xl my-6" >Login</p>
       <form onSubmit = {handleSubmit((data) => {
         // handle async get to db here w/ try + catch
         // if their account doesn't exist, return error and route to sign up page
       })}>
-        <input {...register("username", {required: 'username is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 shadow-2xl" autoFocus placeholder = "Username"/>
+        <input {...register("username", {required: 'username is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 hover:shadow-2xl" autoFocus placeholder = "Username"/>
         <p>{errors.username?.message}</p>
-        <input {...register("email", {required: 'email is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 shadow-xl" placeholder = "Email"/>
+        <input {...register("email", {required: 'email is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 hover:shadow-xl" placeholder = "Email"/>
         <p>{errors.email?.message}</p>
-        <input {...register("password", {required: 'password is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 shadow-xl" placeholder = "Password"/>
+        <input {...register("password", {required: 'password is required'})} className = "box-border h-35 w-3/4 p-4 border-4 cursor-pointer bg-cover bg-center mb-2 hover:border-gray-400 border-4 hover:shadow-xl" placeholder = "Password"/>
         <p>{errors.password?.message}</p>
-        <input type = "submit" className="text-lg cursor-pointer bg-cover bg-center" />
+        <input type = "submit" className="text-lg cursor-pointer bg-cover bg-center hover:underline" />
       </form>
       </div>
     </div>
