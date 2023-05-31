6caaa88d694613dea570dfe1fac2ad6bf38a1f1c
achen9149 <aaronchen9149@gmail.com>
2023-05-31 16:57:15 -0400

    error message for form group name

--- src/app/FormBuilder/page.tsx
+++ src/app/FormBuilder/page.tsx
@@ -3,14 +3,18 @@ import { useState } from "react";
 import Customizers from "../(components)/Customizers";
 import TSBuilder from "../(components)/TSBuilder"
 import HTMLBuilder from "../(components)/HTMLBuilder";
-import { JsxElement } from "typescript";
 
 export default function FormBuilder () {
   //detects when reset button is pressed by incrementing the numericla value 
   //allow child component to detch when the button is pressed by looking at the value/count
   const [pressResetButton, setPressResetButton] = useState<number>(0);
 
-  const [formGroupName, setFormGroupName] = useState('');
+  //stores group name
+  const [formGroupName, setFormGroupName] = useState<string>('');
+  const [isTouched, setIsTouched] = useState<boolean>(false);
+  const handleBlur = () => {
+    setIsTouched(true)
+  };
 
   //this is where state is first defined for the form configuration
   //the formControl Arrray is going to contain all the html components and initialize to an empty array
@@ -32,8 +36,11 @@ export default function FormBuilder () {
         <div className="flex flex-col w-1/2 text-center mt-5">
           <label htmlFor="formGroup">Form Group Name</label>
           <input className="border border-black rounded-md p-2" 
-          onChange={(e) => setFormGroupName(e.target.value)}
-          name="formGroup" type="text" />
+            onChange={(e) => setFormGroupName(e.target.value)}
+            name="formGroup" type="text" 
+            onBlur={handleBlur}
+          />
+        { (formGroupName.length < 1) && (isTouched === true) && <p className="text-red-400"> Form Group is a required field</p>}
         </div>
         <button className="p-2 border border-black rounded-md hover:bg-red-400 duration-500"
           onClick={() => {
