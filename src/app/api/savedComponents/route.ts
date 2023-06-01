b2f102246d3787ecb845ffe6c368fc592d59a535
achen9149 <aaronchen9149@gmail.com>
2023-06-01 19:58:00 -0400

    save code into sql but doesn't preserve white space

--- src/app/api/savedComponents/route.ts
+++ src/app/api/savedComponents/route.ts
@@ -1,8 +1,23 @@
 
 import { NextRequest, NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient();
 
 export async function POST (req: Request, res:Response) {
+  const dataInPost = await req.json();
+  try {
+    const newSavedComponent = await prisma.SavedComponents.create({
+      data: {
+        html: dataInPost.htmlCode,
+        typescript: dataInPost.tsCode,
+        user: {connect: { id: 51 }}
+      }
+    })
 
-  return NextResponse.json({ message: "Hello World" });
+    return NextResponse.json({ message: 'successfully saved form component' }, { status: 200 });
+  } catch(error) {
+    console.log(error)
+    return NextResponse.json({ error: error }, { status: 500 })
+  }
 
 }
