1ba35a853851dff435b223bdb4d9b92774b2c528
ryan h <rhaasti@gmail.com>
2023-06-01 21:57:38 -0400

    added json web tokens to login

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,10 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { PrismaClient } from '@prisma/client'
+import jwt from 'jsonwebtoken';
+
+
+let KEY = process.env.JWT_KEY;
 
 const prisma = new PrismaClient()
 
 
-export async function POST(req: NextRequest) {
+export async function POST(req: NextRequest, res: NextResponse) {
 
   const dataInPost = await req.json()
 
@@ -23,22 +27,28 @@ export async function POST(req: NextRequest) {
       console.log(newUser)
   
   
-      return NextResponse.json({ message: 'success' }, { status: 200 })
+      return NextResponse.json({ message: 'success', status: 200 })
   
     } catch(error) {
       console.log(error)
-      return NextResponse.json({ error: error }, { status: 500 })
+      return NextResponse.json({ error: error, status: 500 })
     }
 
   }
 
   if (dataInPost.type === 'log in') {
 
+    console.log('in the log in POST')
+
       try {
+
+        console.log('in the try')
     
         const findUser = await prisma.user.findFirst({ 
           where: { OR: [{username: dataInPost.usernameEmail},{email: dataInPost.usernameEmail}]}
         })
+
+        console.log('here is the found user:', findUser)
 
 
         if (findUser !== null) {
@@ -46,19 +56,31 @@ export async function POST(req: NextRequest) {
           if (dataInPost.password === findUser.password) {
 
             console.log(findUser)
-            return NextResponse.json({ message: 'success' }, { status: 200 })
+
+            const payload = {
+              id: findUser.id
+            }
+
+            const cookie = jwt.sign(payload, KEY, {expiresIn: 31556926})
+
+            console.log(typeof cookie)
+            
+            return NextResponse.json({ message: 'success', status:200, body: cookie })
 
           } else {
-            return NextResponse.json({ message: 'log in request failed' }, { status: 401 })
+            return NextResponse.json({ error: 'log in request failed', status: 401 })
           }
 
         } else {
-          return NextResponse.json({ message: 'log in request failed' }, { status: 401 })
+
+          console.log('user is null, therefore we are in the else')
+
+          return NextResponse.json({ error: 'log in request failed', status: 401 })
         }
   
       } catch(error) {
         console.log(error)
-        return NextResponse.json({ error: error }, { status: 401 })
+        return NextResponse.json({ error: error, status: 401 })
       }
 
   }
