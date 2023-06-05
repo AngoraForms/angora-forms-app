dc92305f39c20cacb8835b91ec6dbcd25b760080
HWein8 <hadariweinstein@gmail.com>
2023-06-05 17:06:33 -0400

    added some button links on splash page and updated docs

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -35,11 +35,11 @@ export default function Signup() {
   }
 
   return (
-    <div>
+    <div className="bg-primary">
       <div className = 'flex justify-center items-center h-screen'>
       <div className = 'h-1/2 w-1/2 justify-items-center'>
       <form className = 'min-h-full w-full space-y-4' onSubmit={handleSubmit}>
-      <p className = 'text-3xl my-6'>Sign Up</p>
+      <p className = 'text-3xl my-6 text-white'>Sign Up</p>
         <div>
           <label htmlFor = 'username'></label>
           <input type = 'text' required id = 'username' className = 'box-border h-1/10 w-full p-4 border-4 cursor-pointer bg-cover bg-center mb-2 rounded-lg hover:border-gray-400 border-4 hover:shadow-2xl' autoFocus placeholder = 'Username'/>
@@ -52,7 +52,7 @@ export default function Signup() {
           <label htmlFor = 'password' ></label>
           <input type = 'text' required id = 'password' className = 'box-border h-1/10 w-full p-4 border-4 cursor-pointer bg-cover bg-center mb-2 rounded-lg hover:border-gray-400 border-4 hover:shadow-2xl' autoFocus placeholder = 'Password'/>
         </div>
-        <button type = 'submit' className = 'text-lg cursor-pointer bg-transparent hover:to-gray-300 hover:ring-2 hover:body-gray-300 hover:outline-none py-2 px-4 rounded-full'>Submit</button>
+        <button type = 'submit' className = 'text-white text-lg cursor-pointer bg-transparent hover:to-gray-300 hover:ring-2 hover:body-gray-300 hover:outline-none py-2 px-4 rounded-full'>Submit</button>
       </form>
       </div>
         </div>
