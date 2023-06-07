471395efa728b4eacbe6da8f024a9692101501ab
Curtis.Lovrak <curtislovrak@gmail.com>
2023-06-07 16:12:51 -0400

    names in footer now in alphabetical order by first name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Link from "next/link";
@@ -8,6 +7,7 @@ import githubLogo from './github.png';
 
 
 const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata = {
   title: 'Create Next App',
@@ -64,6 +64,18 @@ export default function RootLayout({
               <span className="mr-4 md:mr-4">Connect With Our Team :</span>
               <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li className="flex items-center mr-4">
+                  <a href="https://www.linkedin.com/in/aaron-c-335b48127/" className="hover:underline flex items-center">
+                    <Image
+                      className="mr-1"
+                      src={linkedinLogo}
+                      width={14}
+                      height={14}
+                      alt="Linkedin logo"
+                    />
+                    Aaron Chen
+                  </a>
+                </li>
+                <li className="flex items-center mr-4">
                   <a href="https://www.linkedin.com/in/CurtisLovrak/" className="hover:underline flex items-center">
                     <Image
                       className="mr-1"
@@ -99,18 +111,6 @@ export default function RootLayout({
                     Ryan Hastings
                   </a>
                 </li>
-                <li className="flex items-center mr-4">
-                  <a href="https://www.linkedin.com/in/aaron-c-335b48127/" className="hover:underline flex items-center">
-                    <Image
-                      className="mr-1"
-                      src={linkedinLogo}
-                      width={14}
-                      height={14}
-                      alt="Linkedin logo"
-                    />
-                    Aaron Chen
-                  </a>
-                </li>
                 <li className="flex items-center">
                   <a href="https://www.linkedin.com/in/wayne-leung-1242422a/" className="hover:underline flex items-center">
                     <Image
@@ -131,4 +131,3 @@ export default function RootLayout({
     </html>
   )
 }
-
