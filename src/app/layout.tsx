93c472ac6f6faf715d82e3c2c054c652070e5f46
waynetwleung <waynetwleung@gmail.com>
2023-07-02 18:25:51 -0400

    updated branding and other minor semantics changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,7 @@ import { Providers } from './GlobalRedux/provider'
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Angora Forms',
+  title: 'AngoraForms',
   description: 'Generate and save your custom Angular forms',
 };
 
@@ -22,35 +22,38 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" 
-      style={{ height: '100%' }}
-    >
+    <html lang="en" style={{ height: '100%' }}>
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0"
+        />
       </head>
-      <body className={inter.className}
+      <body
+        className={inter.className}
         style={{
           position: 'relative',
           margin: 0,
-          minHeight: '100%'
+          minHeight: '100%',
         }}
       >
         <nav className="fixed top-0 w-screen bg-white p-6 shadow-xl z-50">
           <Providers>
-            <NavBar/>
+            <NavBar />
           </Providers>
         </nav>
-        <div> 
-          <Providers>
-            {children}
-          </Providers>
+        <div>
+          <Providers>{children}</Providers>
         </div>
         <footer className="bg-white rounded-t-lg shadow p-4 absolute bottom-0 left-0 right-0 z-40">
           <div className="w-full mx-auto max-w-screen-xl p-4 flex flex-col md:flex-row md:items-center md:justify-between">
             <div className="flex flex-col md:flex-row md:items-center">
               <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li className="flex items-center mr-4">
-                  <a href="https://github.com/oslabs-beta/angora-forms" className="hover:underline flex items-center">
+                  <a
+                    href="https://github.com/oslabs-beta/angora-forms"
+                    className="hover:underline flex items-center"
+                  >
                     <Image
                       className="mr-2"
                       src={githubLogo}
@@ -58,11 +61,14 @@ export default function RootLayout({
                       height={22}
                       alt="Github logo"
                     />
-                    Angora Forms
+                    AngoraForms
                   </a>
                 </li>
                 <li className="flex items-center mr-4">
-                  <a href="https://medium.com/@wayneleung_2900/making-angular-custom-form-components-easier-to-work-with-e2f7ace48cb2" className="hover:underline flex items-center">
+                  <a
+                    href="https://medium.com/@wayneleung_2900/making-angular-custom-form-components-easier-to-work-with-e2f7ace48cb2"
+                    className="hover:underline flex items-center"
+                  >
                     <Image
                       className="mr-2"
                       src={mediumLogo}
@@ -79,7 +85,10 @@ export default function RootLayout({
               <span className="mr-4 md:mr-4">Connect With Our Team</span>
               <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                 <li className="flex items-center mr-4">
-                  <a href="https://www.linkedin.com/in/aaron-c-335b48127/" className="hover:underline flex items-center">
+                  <a
+                    href="https://www.linkedin.com/in/aaron-c-335b48127/"
+                    className="hover:underline flex items-center"
+                  >
                     <Image
                       className="mr-1"
                       src={linkedinLogo}
@@ -91,7 +100,40 @@ export default function RootLayout({
                   </a>
                 </li>
                 <li className="flex items-center mr-4">
-                  <a href="https://www.linkedin.com/in/CurtisLovrak/" className="hover:underline flex items-center">
+                  <a
+                    href="https://www.linkedin.com/in/ryan-hastings-50b70457/"
+                    className="hover:underline flex items-center"
+                  >
+                    <Image
+                      className="mr-1"
+                      src={linkedinLogo}
+                      width={14}
+                      height={14}
+                      alt="Linkedin logo"
+                    />
+                    Ryan Hastings
+                  </a>
+                </li>
+                <li className="flex items-center mr-4">
+                  <a
+                    href="https://www.linkedin.com/in/wayne-leung-1242422a/"
+                    className="hover:underline flex items-center"
+                  >
+                    <Image
+                      className="mr-1"
+                      src={linkedinLogo}
+                      width={14}
+                      height={14}
+                      alt="Linkedin logo"
+                    />
+                    Wayne Leung
+                  </a>
+                </li>
+                <li className="flex items-center mr-4">
+                  <a
+                    href="https://www.linkedin.com/in/CurtisLovrak/"
+                    className="hover:underline flex items-center"
+                  >
                     <Image
                       className="mr-1"
                       src={linkedinLogo}
@@ -103,7 +145,10 @@ export default function RootLayout({
                   </a>
                 </li>
                 <li className="flex items-center mr-4">
-                  <a href="https://www.linkedin.com/in/hadarweinstein/" className="hover:underline flex items-center">
+                  <a
+                    href="https://www.linkedin.com/in/hadarweinstein/"
+                    className="hover:underline flex items-center"
+                  >
                     <Image
                       className="mr-1"
                       src={linkedinLogo}
@@ -112,30 +157,6 @@ export default function RootLayout({
                       alt="Linkedin logo"
                     />
                     Hadar Weinstein
-                  </a>
-                </li>
-                <li className="flex items-center mr-4">
-                  <a href="https://www.linkedin.com/in/ryan-hastings-50b70457/" className="hover:underline flex items-center">
-                    <Image
-                      className="mr-1"
-                      src={linkedinLogo}
-                      width={14}
-                      height={14}
-                      alt="Linkedin logo"
-                    />
-                    Ryan Hastings
-                  </a>
-                </li>
-                <li className="flex items-center">
-                  <a href="https://www.linkedin.com/in/wayne-leung-1242422a/" className="hover:underline flex items-center">
-                    <Image
-                      className="mr-1"
-                      src={linkedinLogo}
-                      width={14}
-                      height={14}
-                      alt="Linkedin logo"
-                    />
-                    Wayne Leung
                   </a>
                 </li>
               </ul>
